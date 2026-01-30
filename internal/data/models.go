250d3d8bda0511fe9dee24a6d345d7c89d4d555b
azizjon12 <haknazarov.aziz@mail.ru>
2026-01-30 10:04:53 -0500

    user model setup and registration

--- internal/data/models.go
+++ internal/data/models.go
@@ -13,11 +13,13 @@ var (
 // Wraps the MovieModel. Other models like UserModel, PermissionModel will be added
 type Models struct {
 	Movies MovieModel
+	Users  UserModel
 }
 
-// Returns a Models struct containing the initialized MovieModel
+// Returns a Models struct containing the initialized MovieModel and others
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db},
 	}
 }
